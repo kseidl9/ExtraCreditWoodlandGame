dc10640b6a378f9d3ce0f76c30d24492ee83a132
Katie Seidl <katie.seidl@comcast.net>
2019-06-04 23:50:06 -0700

    copied bunny class over and based deer off of it made create Deer method to be called when the game sets up and it will put one deer in a random position on the screen

---
+++ src/Bunny.java
@@ -0,0 +1,103 @@
+import processing.core.PImage;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+public class Bunny extends AbstractMobileEntity{
+
+    private static final String CARROT_KEY = "carrot";
+    private static final String BUNNY_KEY = "bunny";
+    //private boolean letThereBeBunnies = false;
+
+
+    public Bunny(String id, Point position, List<PImage> images, int actionPeriod, int animationPeriod) {
+        super(id, position, images, actionPeriod, animationPeriod);
+    }
+
+    /*public void letThereBeBunnies(){
+        letThereBeBunnies = true;
+    }
+    */
+
+    @Override
+    public Point nextPosition(WorldModel world, Point destPos) {
+        Point position = getPosition();
+        PathingStrategy strategy = getStrategy();
+        List<Point> pts = strategy.computePath(position, destPos,
+                p -> !world.isOccupied(p),
+                (s,d) -> s.adjacent(d),
+                PathingStrategy.DIAGONAL_CARDINAL_NEIGHBORS);
+        if (pts.isEmpty())
+            return position;
+        else
+            return pts.get(pts.size() -2);
+    }
+
+    @Override
+    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
+        Optional<Entity> minerTarget = world.findNearest(this.getPosition(), Miner.class);
+        long nextPeriod = this.getActionPeriod();
+
+        if (minerTarget.isPresent())
+        {
+            Point tgtPos = minerTarget.get().getPosition();
+
+            if (moveTo(world, (Miner)minerTarget.get(), scheduler))
+            {
+                //Carrot carrot = new Carrot("carrot", tgtPos, imageStore.getImageList(CARROT_KEY));
+                //world.addEntity(carrot);
+                nextPeriod += this.getActionPeriod();
+            }
+        }
+
+        scheduler.scheduleEvent(this,
+                new ActivityAction(this, world, imageStore),
+                nextPeriod);
+    }
+
+    public static void spawnBunnies(WorldModel world, Point p, ImageStore imageStore, EventScheduler scheduler){
+        //spawn bunnies
+        System.out.print("here");
+        Random rand = new Random();
+        int nbuns = rand.nextInt(20)+2;
+        nbuns = 1;
+        for (int i = 0; i < nbuns; i++ ){
+            //Point close = new Point(p.x + rand.nextInt(7), p.y + rand.nextInt(7));
+            //System.out.println(close);
+            Bunny bun = new Bunny("bunny",p, imageStore.getImageList(BUNNY_KEY), 6,5 );
+
+
+            world.addEntity(bun);
+            bun.scheduleActions(scheduler, world, imageStore);
+            System.out.print("bun");
+        }
+
+    }
+    private boolean moveTo(WorldModel world, Miner target, EventScheduler scheduler)
+    {
+        if (this.getPosition().adjacent(target.getPosition()))
+        {
+            world.removeEntity(target);
+            scheduler.unscheduleAllEvents(target);
+            return true;
+        }
+        else
+        {
+            Point nextPos = this.nextPosition(world, target.getPosition());
+
+            if (!this.getPosition().equals(nextPos))
+            {
+                Optional<Entity> occupant = world.getOccupant(nextPos);
+                if (occupant.isPresent())
+                {
+                    scheduler.unscheduleAllEvents(occupant.get());
+                }
+
+                world.moveEntity(this, nextPos);
+            }
+            return false;
+        }
+    }
+}
---
+++ src/Deer.java
@@ -0,0 +1,94 @@
+import processing.core.PImage;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.awt.*;
+
+public class Deer extends AbstractMobileEntity {
+
+        private static final String DEER_KEY = "deer";
+        //private boolean letThereBeBunnies = false;
+
+
+        public Deer(String id, Point position, List<PImage> images, int actionPeriod, int animationPeriod) {
+            super(id, position, images, actionPeriod, animationPeriod);
+        }
+
+
+        @Override
+        public Point nextPosition(WorldModel world, Point destPos) {
+            Point position = getPosition();
+            PathingStrategy strategy = getStrategy();
+            List<Point> pts = strategy.computePath(position, destPos,
+                    p -> !world.isOccupied(p),
+                    (s,d) -> s.adjacent(d),
+                    PathingStrategy.DIAGONAL_CARDINAL_NEIGHBORS);
+            if (pts.isEmpty())
+                return position;
+            else
+                return pts.get(pts.size() -2);
+        }
+
+        @Override
+        public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
+            Optional<Entity> bunnyTarget = world.findNearest(this.getPosition(), Bunny.class);
+            long nextPeriod = this.getActionPeriod();
+
+            if (bunnyTarget.isPresent())
+            {
+                Point tgtPos = bunnyTarget.get().getPosition();
+
+                if (moveTo(world, (Bunny)bunnyTarget.get(), scheduler))
+                {
+                    //bunny follows deer
+                    nextPeriod += this.getActionPeriod();
+                }
+            }
+
+            scheduler.scheduleEvent(this,
+                    new ActivityAction(this, world, imageStore),
+                    nextPeriod);
+        }
+
+        public static void createDeer(WorldModel world, ImageStore imageStore, EventScheduler scheduler){
+            //for when game starts
+            System.out.print("here");
+            Random rand = new Random();
+            Point start = new Point(rand.nextInt(32), rand.nextInt(32));
+
+            Deer deer = new Deer(DEER_KEY,start, imageStore.getImageList(DEER_KEY), 6,5 );
+
+
+            world.addEntity(deer);
+            deer.scheduleActions(scheduler, world, imageStore);
+            System.out.print("deer");
+            }
+
+
+        private boolean moveTo(WorldModel world, Bunny target, EventScheduler scheduler)
+        {
+            if (this.getPosition().adjacent(target.getPosition()))
+            {
+                world.removeEntity(target);
+                scheduler.unscheduleAllEvents(target);
+                return true;
+            }
+            else
+            {
+                Point nextPos = this.nextPosition(world, target.getPosition());
+
+                if (!this.getPosition().equals(nextPos))
+                {
+                    Optional<Entity> occupant = world.getOccupant(nextPos);
+                    if (occupant.isPresent())
+                    {
+                        scheduler.unscheduleAllEvents(occupant.get());
+                    }
+
+                    world.moveEntity(this, nextPos);
+                }
+                return false;
+            }
+        }
+    }
