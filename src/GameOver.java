404ec584be33669b41a01e20e4696e4a3b84ba3b
rbhatt01 <46603395+rbhatt01@users.noreply.github.com>
2019-06-06 01:07:17 -0700

    Create GameOver.java
    created new class

---
+++ src/GameOver.java
@@ -0,0 +1,9 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public class GameOver extends AbstractEntity {
+    public GameOver(String id, Point position, List<PImage>images) {
+        super(id, position, images);
+    }
+}
