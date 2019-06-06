302796dcd355a218ca913759114969e9bdc7919a
Katie Seidl <katie.seidl@comcast.net>
2019-06-06 00:40:09 -0700

    added maybe kill fox edited next position to only move towards the train of bunnies edited parameters of moveTo

--- src/Fox.java
+++ src/Fox.java
@@ -1,9 +1,6 @@
 import processing.core.PImage;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
-import java.awt.*;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -12,39 +9,47 @@ public class Fox extends AbstractMobileEntity {
 
         private static final String CARROT_KEY = "carrot";
         private static final String FOX_KEY = "fox";
-        //private boolean letThereBeBunnies = false;
+        private Entity target = null;
 
 
         public Fox(String id, Point position, List<PImage> images, int actionPeriod, int animationPeriod) {
             super(id, position, images, actionPeriod, animationPeriod);
         }
 
+        public static void maybeKillFox(WorldModel world, Point mouse, ImageStore imageStore, EventScheduler scheduler){
+            if (world.isOccupied(mouse)){
+                if (world.getOccupant(mouse).getClass().equals(Fox.class)){
+                    Fox fox = (Fox)world.getOccupant(mouse).get();
+                    world.removeEntity(fox);
+                }
+            }
+        }
         @Override
         public Point nextPosition(WorldModel world, Point destPos) {
-            Point position = getPosition();
-            PathingStrategy strategy = getStrategy();
-            List<Point> pts = strategy.computePath(position, destPos,
-                    p -> !world.isOccupied(p),
-                    (s,d) -> s.adjacent(d),
-                    PathingStrategy.DIAGONAL_CARDINAL_NEIGHBORS);
+            List<Point> pts = new LinkedList<>();
+            if (target != null) {
+                Point pos = target.getPosition();
+                PathingStrategy strategy = getStrategy();
+                pts = strategy.computePath(getPosition(), pos, p -> !world.isOccupied(p),
+                        (s, d) -> s.adjacent(d),
+                        PathingStrategy.DIAGONAL_CARDINAL_NEIGHBORS);
+            }
             if (pts.isEmpty())
-                return position;
+                return getPosition();
             else
-                return pts.get(pts.size() -2);
+                return pts.get(0);
         }
 
         @Override
         public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
-            Optional<Entity> bunnyTarget = world.findNearest(this.getPosition(), Bunny.class);
+            //Optional<Entity> bunnyTarget = world.findNearest(this.getPosition(), Bunny.class);
             long nextPeriod = this.getActionPeriod();
-
-            if (bunnyTarget.isPresent())
+//TODO need to remember to change deer to call set target to last bunny in bunny list when ria is done
+            if (target!= null)
             {
-                Point tgtPos = bunnyTarget.get().getPosition();
-
-                if (moveTo(world, (Bunny)bunnyTarget.get(), scheduler))
+                if (moveTo(world, target, scheduler))
                 {
-                    Carrot carrot = new Carrot("carrot", tgtPos, imageStore.getImageList(CARROT_KEY));
+                    Carrot carrot = new Carrot("carrot", target.getPosition(), imageStore.getImageList(CARROT_KEY));
                     world.addEntity(carrot);
                     nextPeriod += this.getActionPeriod();
                 }
@@ -55,23 +60,8 @@ public class Fox extends AbstractMobileEntity {
                     nextPeriod);
         }
 
-        public static void spawnFoxes(WorldModel world, Point p, ImageStore imageStore, EventScheduler scheduler){
 
-            Random rand = new Random();
-            int foxies = rand.nextInt(3)+1;
-            for (int i = 0; i < foxies; i++ ){
-                Point close = new Point(p.x + rand.nextInt(7), p.y + rand.nextInt(7));
-
-                Fox fox = new Fox(FOX_KEY,close, imageStore.getImageList(FOX_KEY), 6,5 );
-
-
-                world.addEntity(fox);
-                fox.scheduleActions(scheduler, world, imageStore);
-
-            }
-
-        }
-        private boolean moveTo(WorldModel world, Bunny target, EventScheduler scheduler)
+        private boolean moveTo(WorldModel world, Entity target, EventScheduler scheduler)
         {
             if (this.getPosition().adjacent(target.getPosition()))
             {
@@ -96,5 +86,21 @@ public class Fox extends AbstractMobileEntity {
                 return false;
             }
         }
+    public static void spawnFoxes(WorldModel world, Point p, ImageStore imageStore, EventScheduler scheduler){
+
+        Random rand = new Random();
+        int foxies = rand.nextInt(3)+1;
+        for (int i = 0; i < foxies; i++ ){
+            Point close = new Point(p.x + rand.nextInt(7), p.y + rand.nextInt(7));
+
+            Fox fox = new Fox(FOX_KEY,close, imageStore.getImageList(FOX_KEY), 6,5 );
+
+
+            world.addEntity(fox);
+            fox.scheduleActions(scheduler, world, imageStore);
+
+        }
+
+    }
     }
 
